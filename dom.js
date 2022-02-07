62ed2233d86293fdb71bc523272881ba19ee40f9
yja90 <74431763+yja90@users.noreply.github.com>
2022-02-07 14:55:26 +0000

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,9 +1,25 @@
+//create button that allows you to switch between rainbow and black/White
+//need to remove event listener after second click
+
+
+
+//Global variables
 const mainContainer = document.querySelector('#mainContainer');
-let grid
+let gridColor = document.getElementsByClassName('row')
+const grid16 = document.querySelector('#grid16');
+const grid32 = document.querySelector('#grid32');
+const grid64 = document.querySelector('#grid64');
+const resetGrid = document.querySelector('#resetGrid');
+const blackWhiteColor = document.querySelector('#blackWhiteColor')
+const rubber = document.querySelector('#rubber')
+const rainbowColors = document.querySelector('#rainbowColors')
 let gridSize = 0
 let totalGridSize
-let gridColor = document.getElementsByClassName('row')
-// let random_color
+
+
+
+
+
 
 createGrid(16, '2.5vw')
 
@@ -12,23 +28,50 @@ createGrid(16, '2.5vw')
 // creates Grid and assigns the correct style
 function createGrid(gridSize, style) {
   for (i = 0; i < (gridSize * gridSize); ++i) {
-    grid = document.createElement('div')
-    // rows.setAttribute("id", "mainGrid");
+    let grid = document.createElement('div')
     mainContainer.appendChild(grid).className = 'row';
     grid.style.width = style
     grid.style.height = style
-    gridColor[i].addEventListener('mouseover', changeColor)
+    gridColor[i].addEventListener('click', () => {
+      color('#484848')
+    })
   }
   totalGridSize = mainContainer.childElementCount
 }
 
+function rainbowColor (){
+  for (i = 0; i < (totalGridSize); ++i) {
+    gridColor[i].addEventListener('mouseover', function() {
+      let colors = ['#ff3366', '#ff6633', '#FFCC33', '#33FF66', '#33FFCC', '#33CCFF', '#3366FF', '#6633FF', '#CC33FF'];
+      let randomColor = colors[Math.floor(Math.random() * colors.length)];
+      this.style.backgroundColor = randomColor
 
-//Function to change background color of div elements
-function changeColor() {
-  let colors = ['#ff3366', '#ff6633', '#FFCC33', '#33FF66', '#33FFCC', '#33CCFF', '#3366FF', '#6633FF', '#CC33FF'];
-  let random_color = colors[Math.floor(Math.random() * colors.length)];
-  this.style.backgroundColor = random_color
+    })
+}}
 
+function removeEvent (){
+  gridColor.addEventListener('click',color, false)
+}
+
+blackWhiteColor.addEventListener('click', () => {
+  color('#484848')
+})
+rubber.addEventListener('click', () => {
+  color('#e6e6ff')
+  removeEvent ()
+})
+
+rainbowColors.addEventListener('click', rainbowColor)
+
+//Function that allows you to change color of grid
+function color(color) {
+  for (i = 0; i < totalGridSize; ++i) {
+    gridColor[i].addEventListener('mouseover', function() {
+      this.style.backgroundColor = color
+
+    })
+
+  }
 }
 
 
@@ -44,19 +87,19 @@ function clearGrid(totalGridSize) {
 
 
 // Button to change grid size
-const grid16 = document.querySelector('#grid16');
+
 grid16.addEventListener('click', () => {
   clearGrid(totalGridSize)
   createGrid(16, '2.5vw');
 })
 
-const grid32 = document.querySelector('#grid32');
+
 grid32.addEventListener('click', () => {
   clearGrid(totalGridSize)
   createGrid(32, '1.25vw');
 })
 
-const grid64 = document.querySelector('#grid64');
+
 grid64.addEventListener('click', () => {
   clearGrid(totalGridSize)
   createGrid(64, '0.625vw');
@@ -64,7 +107,6 @@ grid64.addEventListener('click', () => {
 
 
 //Button to reset grid
-const resetGrid = document.querySelector('#resetGrid');
 resetGrid.addEventListener('click', () => {
   clearGrid(totalGridSize)
   createGrid(16, '2.5vw');
